f7e36701beea436a27d8d2c740c00bdad7aef75a
Jayden Kneller <jayden@arrai.com>
2023-02-17 11:40:07 -0700

    test: Add tests mocking where showmigrations and makemigrations return an error.

--- tests/food/tests.py
+++ tests/food/tests.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from tests.test_case import ManagementCommandTestCase
 
@@ -40,3 +41,116 @@ class FoodTestCase(ManagementCommandTestCase):
         self.assertTrue(os.path.exists("tests/food/migrations/0001_initial.py"))
         self.assertTrue(os.path.exists("tests/food/migrations/0002_create_view.py"))
         self.assertTrue(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+        shutil.rmtree("tests/food/sql")
+
+    def test_show_migrations_err_when_creating_initial_migration(self):
+        out, err = self.mock_call_command_returning_an_error("showmigrations", 1, "food_sweets", "create_view")
+        self.assertTupleEqual(err, ())
+        self.assertTupleEqual(
+            out,
+            (
+                "",
+                "Created 'migrations' folder in app 'food'.",
+                "An error occurred.",
+            ),
+        )
+        self.assertTrue(os.path.exists("tests/food/migrations/__init__.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0001_initial.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0002_create_view.py"))
+        self.assertFalse(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+
+    def test_make_migrations_err_when_creating_initial_migration(self):
+        out, err = self.mock_call_command_returning_an_error("makemigrations", 1, "food_sweets", "create_view")
+        self.assertTupleEqual(err, ())
+        self.assertTupleEqual(
+            out,
+            (
+                "",
+                "Created 'migrations' folder in app 'food'.",
+                "",
+                "Creating initial migration for app 'food'.",
+                "An error occurred.",
+            ),
+        )
+        self.assertTrue(os.path.exists("tests/food/migrations/__init__.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0001_initial.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0002_create_view.py"))
+        self.assertFalse(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+
+    def test_show_migrations_err_when_getting_migration_numbers_and_names(self):
+        out, err = self.mock_call_command_returning_an_error("showmigrations", 2, "food_sweets", "create_view")
+        self.assertTupleEqual(err, ())
+        self.assertTupleEqual(
+            out,
+            (
+                "",
+                "Created 'migrations' folder in app 'food'.",
+                "",
+                "Creating initial migration for app 'food'.",
+                "Migrations for 'food':",
+                "tests/food/migrations/0001_initial.py",
+                "- Create model Sweets",
+                "",
+                "Created 'sql' folder in app 'food'.",
+                "An error occurred.",
+            ),
+        )
+        self.assertTrue(os.path.exists("tests/food/migrations/__init__.py"))
+        self.assertTrue(os.path.exists("tests/food/migrations/0001_initial.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0002_create_view.py"))
+        self.assertFalse(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+        shutil.rmtree("tests/food/sql")
+
+    def test_make_migrations_err_when_creating_empty_migration(self):
+        out, err = self.mock_call_command_returning_an_error("makemigrations", 2, "food_sweets", "create_view")
+        self.assertTupleEqual(err, ())
+        self.assertTupleEqual(
+            out,
+            (
+                "",
+                "Created 'migrations' folder in app 'food'.",
+                "",
+                "Creating initial migration for app 'food'.",
+                "Migrations for 'food':",
+                "tests/food/migrations/0001_initial.py",
+                "- Create model Sweets",
+                "",
+                "Created 'sql' folder in app 'food'.",
+                "",
+                "Creating empty migration for the new SQL view.",
+                "An error occurred.",
+            ),
+        )
+        self.assertTrue(os.path.exists("tests/food/migrations/__init__.py"))
+        self.assertTrue(os.path.exists("tests/food/migrations/0001_initial.py"))
+        self.assertFalse(os.path.exists("tests/food/migrations/0002_create_view.py"))
+        self.assertFalse(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+        shutil.rmtree("tests/food/sql")
+
+    def test_show_migrations_err_when_getting_the_new_migration_number_and_name(self):
+        with self.assertRaises(RuntimeError):
+            _out, _err = self.mock_call_command_returning_an_error("showmigrations", 3, "food_sweets", "create_view")
+
+        self.assertTrue(os.path.exists("tests/food/migrations/__init__.py"))
+        self.assertTrue(os.path.exists("tests/food/migrations/0001_initial.py"))
+        self.assertTrue(os.path.exists("tests/food/migrations/0002_create_view.py"))
+        self.assertFalse(os.path.exists("tests/food/sql/view-food_sweets-latest.sql"))
+
+        # Cleanup - We need to delete these folders for the other tests in this class.
+        shutil.rmtree("tests/food/migrations")
+        shutil.rmtree("tests/food/sql")
--- tests/test_case.py
+++ tests/test_case.py
@@ -1,6 +1,9 @@
 import contextlib
+import importlib
 import io
+from unittest import mock
 
+from django.core.management import call_command
 from django.core.management import execute_from_command_line
 from django.test import TransactionTestCase
 
@@ -8,8 +11,8 @@ from django.test import TransactionTestCase
 # Tests cannot be run in parallel.
 class ManagementCommandTestCase(TransactionTestCase):
     def call_command(self, command):
-        err = io.StringIO()
-        out = io.StringIO()
+        self.stderr = err = io.StringIO()
+        self.stdout = out = io.StringIO()
 
         with contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
             try:
@@ -21,3 +24,63 @@ class ManagementCommandTestCase(TransactionTestCase):
         out.seek(0)
 
         return tuple(map(str.strip, out.readlines())), tuple(map(str.strip, err.readlines()))
+
+    def mock_call_command_returning_an_error(self, command, on_which_call, db_table_name, migration_name):
+        """
+        Mocking the first call of 'showmigrations' or 'makemigrations' is easy.
+        Mocking the second, or third call requires us to keep track of how many times it has been called,
+        and we need to run the original call command until we get to the one we want to mock.
+        So this function is here to help with that, and keep it in one place.
+        """
+        self._mock_command_count = 0
+        self._mock_command_on_count = on_which_call
+        self._mock_command = command
+
+        with mock.patch(
+            "django_view_manager.utils.management.commands.makeviewmigration.Command._call_command"
+        ) as mocked_call_command:
+            mocked_call_command.side_effect = self.do_original_call_command_or_err_on_specific_call
+            results = self.call_command(["manage.py", "makeviewmigration", db_table_name, migration_name])
+
+        del self._mock_command_count
+        del self._mock_command_on_count
+        del self._mock_command
+        del self.stdout
+        del self.stderr
+
+        return results
+
+    def do_original_call_command_or_err_on_specific_call(self, *args):
+        """
+        This function replicates the code in the _call_command function
+        in the management command, minus writing in the error style.
+        It also writes an error when we want it to, instead of calling
+        """
+        err = io.StringIO()
+        out = io.StringIO()
+
+        if args[0] == self._mock_command:
+            self._mock_command_count += 1
+
+        # If we are running the correct command to mock, and the count is exactly when we want to call it,
+        # then set err and continue with the code as it would in _call_command in the management command,
+        # otherwise we run the code as it would have in _call_command in the management command.
+        if args[0] == self._mock_command and self._mock_command_count == self._mock_command_on_count:
+            err.write("An error occurred.")
+
+        else:
+            # If we don't do this, sometimes we can't import a newly created migration.
+            # Do it here, so we don't need to know which calls require it, and which don't.
+            importlib.invalidate_caches()
+            with contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
+                call_command(*args)
+
+        # Did an error occur?
+        if err.tell():
+            err.seek(0)
+            self.stdout.write(err.read())
+            return False
+
+        # Return the results.
+        out.seek(0)
+        return out.readlines()
